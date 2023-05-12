14aad61896ad3f2fb690c8e4f7b8431b1e1e9c69
Benjamin Laurent <blaurent@mi-r4-p3.s19.be>
2023-05-12 13:47:30 +0200

    fini pour de vrai

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: blaurent <blaurent@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -13,22 +13,24 @@
 #include "Fixed.hpp"
 #include <iostream>
 
-int main( void ) {
-Fixed a;
-Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-std::cout << a << std::endl;
-std::cout << ++a << std::endl;
-std::cout << a << std::endl;
-std::cout << a++ << std::endl;
-std::cout << a << std::endl;
-std::cout << b << std::endl;
-std::cout << Fixed::max( a, b ) << std::endl;
-std::cout << Fixed::min( a, b ) << std::endl;
-Fixed c(42);
-Fixed d(2);
-std::cout << c << std::endl;
-std::cout << c / d << std::endl;
-std::cout << c * d << std::endl;
-std::cout << c - d << std::endl;
-return 0;
+int main( void )
+{
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::min( a, b ) << std::endl;
+	Fixed c(-21);
+	Fixed d(2);
+	std::cout << " rawbits" << c << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	std::cout << c / d << std::endl;
+	std::cout << c * d << std::endl;
+	std::cout << c - d << std::endl;
+	return 0;
 }
